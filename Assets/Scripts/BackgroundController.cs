cc96b8d07eef2a768305951f0aad63e138ba267b
kamilam1987 <kamilam1987@gmail.com>
2018-10-08 14:38:35 +0100

    Created Assets

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+    public float scrollSpeed ;
+    private Vector2 offset;
+	
+	// Update is called once per frame
+	void Update () {
+        offset = new Vector2(0,Time.time * scrollSpeed);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+		
+	}
+}
