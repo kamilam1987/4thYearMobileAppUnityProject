e770c68e0cb306dd2b0d423f1e3770459833762c
kamilam1987 <kamilam1987@gmail.com>
2018-10-14 20:31:02 +0100

    Added Enemy shooting

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour {
+
+    //Declare variables
+    float bulletSpeed; //The bullet speed
+    Vector2 bulletDirection; //The dorection of a bullet
+    bool isReady; //To know when the bullet direction is set
+
+    //Sets default values in Awake function
+    private void Awake()
+    {
+        bulletSpeed = 5f;
+        isReady = false;
+    }
+
+    //This function sets the bullet's direction
+    public void SetBulletDirection(Vector2 direction)
+    {
+        //Sets the direction normalized, to get an unit vector
+        bulletDirection = direction.normalized;
+        //Sets to true
+        isReady = true;
+    }//End of SetBulletDirection function
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isReady)
+        {
+            //Gets the current position of a bullet
+            Vector2 position = transform.position;
+
+            //Sets the bullet new position
+            position += bulletDirection * bulletSpeed * Time.deltaTime;
+
+            //Updates the new position
+            transform.position = position;
+
+            //The bottom-left point on the screen
+            Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+
+            //The top-right point on the screen
+            Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+            //Destroy bullet if will go outside the screen
+            if ((transform.position.x < min.x) || (transform.position.x > max.x) || (transform.position.y < min.y) || (transform.position.y > max.y))
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/EnemyGun.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGun : MonoBehaviour {
+
+    //Dclare variables
+    public GameObject EnemyBullet; //Enemy bullet prefab
+
+	// Use this for initialization
+	void Start () {
+        //Fires the enemy bullet after 1 second
+        Invoke("FireEnemyBullet", 1f);
+	}
+	
+    //Fires enemy bullet function
+    void FireEnemyBullet() {
+        //Gets a reference to the player's ship
+        GameObject playerShip = GameObject.Find("PlayerShip");
+
+        //If player is not dead
+        if(playerShip != null)
+        {
+            //Instatntiate an enemy bullet
+            GameObject bullet = (GameObject)Instantiate(EnemyBullet);
+
+            //Sets the bullet initial position
+            bullet.transform.position = transform.position;
+
+            //Direction towards the player's ship
+            Vector2 direction = playerShip.transform.position - bullet.transform.position;
+
+            //Sets the bullet direction
+            bullet.GetComponent<EnemyBullet>().SetBulletDirection(direction);
+        }
+
+
+    }//End FireEnemyBullet function
+}
