e6fa9bb0e2d0bc7c0e3f8ebde8381261d0627e57
kamilam1987 <kamilam1987@gmail.com>
2018-11-10 22:17:15 +0000

    Added setting resolutions

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class PauseMenu : MonoBehaviour {
@@ -9,6 +10,30 @@ public class PauseMenu : MonoBehaviour {
     //Declare variables
     public GameObject pauseObj;//Pause object
     private float tempTimeScale;//Value for time scale when pause is on(stops the game)
+    public GameObject pauseMenuHolder;
+    public GameObject optionsMenuHolder;
+
+
+    public Toggle[] resolutionToggles;
+    public Toggle fullscreenToggle;
+    public int[] screenWidths;
+    int activeScreenResIndex;
+
+    void Start()
+    {
+        activeScreenResIndex = PlayerPrefs.GetInt("screen res index");
+        bool isFullscreen = (PlayerPrefs.GetInt("fullscreen") == 1) ? true : false;
+
+
+
+        for (int i = 0; i < resolutionToggles.Length; i++)
+        {
+            resolutionToggles[i].isOn = i == activeScreenResIndex;
+        }
+
+        //Need to be fix
+        //fullscreenToggle.isOn = isFullscreen;
+    }
 
     void Update()
     {
@@ -51,7 +76,8 @@ public class PauseMenu : MonoBehaviour {
     //On Options Button click
     public void OptionsButton()
     {
-
+        pauseMenuHolder.SetActive(false);
+        optionsMenuHolder.SetActive(true);
     }
     //On Quit Button click
     public void QuitButton()
@@ -61,7 +87,48 @@ public class PauseMenu : MonoBehaviour {
         //Exit game
         Application.Quit();
     }
+    //On Back Button click
+    public void BackButton()
+    {
+        //Starts time
+        Time.timeScale = 0;
+        
+        pauseMenuHolder.SetActive(true);
+        optionsMenuHolder.SetActive(false);
+    }
 
+
+    public void SetScreenResolution(int i)
+    {
+        if (resolutionToggles[i].isOn)
+        {
+            activeScreenResIndex = i;
+            float aspectRatio = 16 / 9f;
+            Screen.SetResolution(screenWidths[i], (int)(screenWidths[i] / aspectRatio), false);
+            PlayerPrefs.SetInt("screen res index", activeScreenResIndex);
+            PlayerPrefs.Save();
+        }
+    }
+    public void SetFullScreen(bool isFullscreen)
+    {
+        for (int i = 0; i < resolutionToggles.Length; i++)
+        {
+            resolutionToggles[i].interactable = !isFullscreen;
+        }
+
+        if (isFullscreen)
+        {
+            Resolution[] allResolutions = Screen.resolutions;
+            Resolution maxResolution = allResolutions[allResolutions.Length - 1];
+            Screen.SetResolution(maxResolution.width, maxResolution.height, true);
+        }
+        else
+        {
+            SetScreenResolution(activeScreenResIndex);
+        }
+        PlayerPrefs.SetInt("fullscreen", ((isFullscreen) ? 1 : 0));
+        PlayerPrefs.Save();
+    }
 
 
 }
